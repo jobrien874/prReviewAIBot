16db887326ec39821e10e6315eb276c3a8d5953e
Josh O'Brien <jobrien874@gmail.com>
2024-02-05 17:16:34 +0000

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,12 +4,12 @@ export class Chat {
   constructor(key) {
     this.chatGPTAPI = new ChatGPTAPI({ apiKey: key, apiBaseUrl: 'https://api.openai.com/v1' });
   }
-
   async getResponse(prompt) {
     return await this.chatGPTAPI.sendMessage(prompt);
   }
 
   askQuestion = async (prompt) => {
+    console.log('hello');
     const response = await getResponse(prompt);
     return response?.text;
   }
