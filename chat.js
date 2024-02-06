82f7ba7d58e081270703630771be7f408a36150f
Josh O'Brien <jobrien874@gmail.com>
2024-02-06 19:12:35 +0000

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,8 +1,7 @@
-import { ChatGPTAPI } from 'chatgpt';
-import { createPromptForGpt } from './codeReview';
+const { createPromptForGpt } = require('./codeReview');
 
 const { Chat } = require('./chat');
-
+const ChatGPTAPI = require('chatgpt');
 export class Chat {
   constructor(key) {
     this.chatGPTAPI = new ChatGPTAPI({ apiKey: key, apiBaseUrl: 'https://api.openai.com/v1' });
@@ -12,12 +11,12 @@ export class Chat {
     return await this.chatGPTAPI.sendMessage(prompt);
   }
 
-   askQuestion = async (file) => {
+  askQuestion = async (file) => {
     if (!file) {
       return;
     }
     const prompt = createPromptForGpt(file);
-    const response = await getResponse(prompt);
+    const response = await this.getResponse(prompt);
     return response?.text;
   }
 }
